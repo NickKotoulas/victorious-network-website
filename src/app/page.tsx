562ed548c3f0f322b9c45232269725bb6408bcf9
NickKotoulas <nick@hivetech.biz>
2026-06-17 14:49:50 +0300

    Render site footer on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { SiteHeader } from "@/components/layout/SiteHeader";
+import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeSections } from "@/sections/HomeSections";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <SiteHeader />
       <HomeSections />
+      <SiteFooter />
     </>
   );
 }
