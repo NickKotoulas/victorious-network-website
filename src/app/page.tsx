e9811bf9375162f6c6a06506c79152ef1bbe53b1
NickKotoulas <nick@hivetech.biz>
2026-06-19 22:45:32 +0300

    Enable bilingual landing context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeSections } from "@/sections/HomeSections";
+import { LanguageProvider } from "@/components/providers/LanguageProvider";
 
 export default function Home() {
   return (
-    <>
+    <LanguageProvider>
       <SiteHeader />
       <HomeSections />
       <SiteFooter />
-    </>
+    </LanguageProvider>
   );
 }
