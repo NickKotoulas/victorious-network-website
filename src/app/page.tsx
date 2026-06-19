40d21befca76f92e56fa2ab927446729f8640364
NickKotoulas <nick@hivetech.biz>
2026-06-20 00:12:36 +0300

    Mount global motion foundation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { SiteHeader } from "@/components/layout/SiteHeader";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { HomeSections } from "@/sections/HomeSections";
 import { LanguageProvider } from "@/components/providers/LanguageProvider";
+import { MotionFoundation } from "@/components/motion/MotionFoundation";
 
 export default function Home() {
   return (
     <LanguageProvider>
+      <MotionFoundation />
       <SiteHeader />
       <HomeSections />
       <SiteFooter />
